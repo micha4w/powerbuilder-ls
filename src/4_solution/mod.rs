4800396888bdffc01749bf398b733ff2be61a301
micha4w <micha4w@gmail.com>
2026-09-01 22:35:27 +0200

    chore: move to protox

--- src/4_solution/mod.rs
+++ src/4_solution/mod.rs
@@ -8,5 +8,4 @@ pub use types::*;
 
 pub mod powerbuilder_proto {
     include!(concat!(env!("OUT_DIR"), "/protobuf/powerbuilder.rs"));
-    include!(concat!(env!("OUT_DIR"), "/protobuf/powerbuilder.serde.rs"));
 }
--- src/6_linter/mod.rs
+++ src/6_linter/mod.rs
@@ -3,8 +3,3 @@ mod lint_top_levels;
 mod linter;
 
 pub use linter::*;
-
-pub mod powerbuilder_proto {
-    include!(concat!(env!("OUT_DIR"), "/protobuf/powerbuilder.rs"));
-    include!(concat!(env!("OUT_DIR"), "/protobuf/powerbuilder.serde.rs"));
-}
